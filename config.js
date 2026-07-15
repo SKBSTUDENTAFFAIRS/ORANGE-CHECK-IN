72d0727b1c4d52b15e76074d0fa6607ee11f1970
MEECHOKE <110534878+SKBSTUDENTAFFAIRS@users.noreply.github.com>
2026-07-15 13:31:03 +0700

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // วาง URL ของ GAS Web App ที่ Deploy แล้วระหว่างเครื่องหมายคำพูด
 window.SKB_CONFIG = {
-  API_URL: 'PASTE_YOUR_GAS_WEB_APP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyXlsFOQKV-cjbrzoWH4LIDaEJ_q4UmStONQ5EcYxZvP6ekVf4LjW4OSWzjCKKKYMhS/exec'
 };
