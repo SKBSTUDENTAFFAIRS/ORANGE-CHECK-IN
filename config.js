9731d75dbb4b6b0a48a7604f557356fe37f20e9f
MEECHOKE <110534878+SKBSTUDENTAFFAIRS@users.noreply.github.com>
2026-07-15 13:23:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // วาง URL ของ GAS Web App ที่ Deploy แล้วระหว่างเครื่องหมายคำพูด
 window.SKB_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbx6e93_8GqvB9ceXjg5qSTMmtAIJJRh-Ct6wJM9Kc1Xol91P3cLiVzebgQEygbiAFHi/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwolzlEVv55N1PEjyuLHs_I3JoCBFxu4i1lhV4pUyy7chhL0XxQaa9UfWrRC46s_cmV/exec'
 };
